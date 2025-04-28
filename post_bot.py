2bd435c9b8c0606591c0e9561e5b97c1a665b873
Sipun Dandpat <sipundandpat31@gmail.com>
2025-04-28 19:16:07 +0530

    Add files via upload

---
+++ post_bot.py
@@ -0,0 +1,25 @@
+import asyncio
+from telegram import *
+import schedule
+import time
+
+BOT_TOKEN = "7796728205:AAHxPoT8hWWEK_GA_Wn9zG-O4oez0qgWlRY"
+CHAT_ID = "@vapourwavechannel"
+
+bot = Bot(token = BOT_TOKEN)
+
+async def daily_post():
+    text = "🌞 Good morning! Here’s your daily motivation:\n\n✨ 'Keep pushing forward!'"
+    await bot.send_message(chat_id=CHAT_ID, text=text)
+    print("message send")
+    
+
+def schedule_job():
+    asyncio.run(daily_post())
+
+schedule.every().day.at("18:26").do(schedule_job)
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(30)
---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python post_bot.py
